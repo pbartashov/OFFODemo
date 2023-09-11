5fd72c55e752967910379bcdf060d166428c5306
pbartashov <p.bartashov@icloud.com>
2023-09-11 21:19:08 +1000

    Initial Commit

---
+++ OFFODemo/OFFODemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  OFFODemoApp.swift
+//  OFFODemo
+//
+//  Created by Павел Барташов on 11.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct OFFODemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
